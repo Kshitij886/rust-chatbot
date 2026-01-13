4890db723dadb3ab7391672383edb9c287edf419
Kshitij886 <kshitijkhatri437@gmail.com>
2026-01-13 13:20:44 +0545

    start the tracer using tracing_suscriber

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -9,7 +9,11 @@ pub mod types;
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    //start the tracer 
+    tracing_subscriber::fmt()
+    .with_max_level(tracing::Level::DEBUG)
+    .init();
     let apis = routes::web_api::web_api().await;
-    tracing::debug!("Server is listining to the {:?}", listener);
+    tracing::debug!("Server is listining on {:?}", listener);
     axum::serve(listener, apis).await.unwrap();
 }
